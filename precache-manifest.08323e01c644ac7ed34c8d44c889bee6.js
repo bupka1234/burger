8cf51d6244ffca044c2ddcd584dc3465bea8d3cf
bupka <bupkadeveloper@gmail.com>
2021-11-12 13:23:50 +0800

    Updates

---
+++ precache-manifest.08323e01c644ac7ed34c8d44c889bee6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e79cba68411908be7fd663a8c320c79c",
+    "url": "/burger/index.html"
+  },
+  {
+    "revision": "3519c7505ce10fe657fa",
+    "url": "/burger/static/css/main.17d553b8.chunk.css"
+  },
+  {
+    "revision": "aa9cdaea35016ffb0274",
+    "url": "/burger/static/js/2.3f993e98.chunk.js"
+  },
+  {
+    "revision": "e540cb51dc4f24037f1158e0238ef6f2",
+    "url": "/burger/static/js/2.3f993e98.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3519c7505ce10fe657fa",
+    "url": "/burger/static/js/main.51e3b989.chunk.js"
+  },
+  {
+    "revision": "d4390a2908c1cac1ad60",
+    "url": "/burger/static/js/runtime-main.b5950f3a.js"
+  },
+  {
+    "revision": "b8503d262bffbfb7c67fd6762963e7d1",
+    "url": "/burger/static/media/burger-logo.b8503d26.png"
+  }
+]);
